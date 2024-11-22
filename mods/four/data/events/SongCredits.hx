63fcde6b8f3bf82f0f30a39685a70b6b653173f6
TheWhiteThaer <167879057+TheWhiteThaer@users.noreply.github.com>
2024-11-23 01:33:18 +0300

    Added song credit event

---
+++ mods/four/data/events/SongCredits.hx
@@ -0,0 +1,25 @@
+import funkin.game.PlayState;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+
+
+var SongCredit:FlxSprite;
+
+function create() {
+    SongCredit = new FlxSprite();
+    SongCredit.cameras = [camHUD];
+    SongCredit.alpha = 0;
+    add(SongCredit);
+       
+
+}
+
+function onEvent(e:EventGameEvent){
+    if (e.event.name == "SongCredits"){
+        var e = e;
+        SongCredit.loadGraphic(Paths.image('game/SongCredits/' + e.event.params[0]));
+        SongCredit.screenCenter();
+        FlxTween.tween(SongCredit, {alpha: 1}, 0.2, {startDelay: 2});
+        FlxTween.tween(SongCredit, {alpha: 0}, 0.2, {startDelay: 4});
+    }
+}
